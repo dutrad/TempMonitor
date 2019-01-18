7169fef488f9b8a1ae631cfff6b7d990b5e65533
Vinicius Dutra Dias <dutrad.vinicius@gmail.com>
2019-01-17 22:52:57 -0200

    Get Temp and save to file

--- TempMonitor/Program.cs
+++ TempMonitor/Program.cs
@@ -1,15 +1,45 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
+using System.IO.Ports;
+using System.IO;
 
 namespace TempMonitor
 {
     class Program
     {
+        const string PORT = "COM4";
+        const string TEMP_COMMAND = "T";
+        static SerialPort serialPort;
+        static string readString;
+        const string FILE = "Temp.txt";
+
         static void Main(string[] args)
         {
+            serialPort = new SerialPort(PORT, 9600);
+            serialPort.Open();
+
+            while(true)
+            {
+                readString = GetTemp();
+                WriteToFile(readString);
+                Thread.Sleep(5000)
+            }
+        }
+
+        static string GetTemp()
+        {
+            serialPort.Write(TEMP_COMMAND);
+            return serialPort.ReadLine();
+        }
+
+        static void WriteToFile(string temp)
+        {
+            try {
+                StreamWriter stream = new StreamWriter(FILE, true);
+                stream.WriteLine(temp.Trim() + "\t" + DateTime.Now);
+                stream.Close();
+            }
+            catch (Exception) { }
         }
     }
 }
