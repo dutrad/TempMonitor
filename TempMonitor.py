d473701c23b593734f7c5a4f8b16c513184a48be
dutrad.vinicius <dutrad.vinicius@gmail.com>
2020-03-21 17:21:07 +0000

    Find available ports

--- TempMonitor.py
+++ TempMonitor.py
@@ -1,7 +1,47 @@
-import serial
+import glob
+import sys
 import time
 
-s = serial.Serial('COM3', 9600, timeout=1)
+import serial
+
+
+def serial_ports():
+    """ Find serial port
+
+        :raises EnvironmentError:
+            On unsupported or unknown platforms
+        :returns:
+            A list of the serial ports available on the system
+    """
+    if sys.platform.startswith('win'):
+        ports = ['COM%s' % (i + 1) for i in range(256)]
+    elif sys.platform.startswith('linux') or sys.platform.startswith('cygwin'):
+        # this excludes your current terminal "/dev/tty"
+        ports = glob.glob('/dev/tty[A-Za-z]*')
+    elif sys.platform.startswith('darwin'):
+        ports = glob.glob('/dev/tty.*')
+    else:
+        raise EnvironmentError('Unsupported platform')
+
+    for port in ports:
+        try:
+            se = serial.Serial(port)
+            se.close()
+            return port
+        except (OSError, serial.SerialException):
+            pass
+
+    return ""
+
+
+sport: str = serial_ports()
+if sport:
+    print(sport + ' will be used')
+else:
+    print('No serial port found')
+    exit()
+
+s = serial.Serial(sport, 9600, timeout=1)
 
 while True:
     s.write(b'T')
