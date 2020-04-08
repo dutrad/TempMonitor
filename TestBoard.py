ea518f0243e63b948d0b8af88c5712dbead609f5
root <dutrad.vincius@gmail.com>
2020-04-08 11:29:26 +0100

    Python script to test board

---
+++ TestBoard.py
@@ -0,0 +1,62 @@
+import glob
+import sys
+import time
+import urllib.parse
+import urllib.request
+
+from serial import Serial, SerialException
+
+
+def serial_ports() -> str:
+    """ Find serial port
+
+        :raises EnvironmentError:
+            On unsupported or unknown platforms
+        :returns:
+            A list of the serial ports available on the system
+    """
+    if sys.platform.startswith('win'):
+        ports = ['COM%s' % (i + 1) for i in range(256)]
+    elif sys.platform.startswith('linux') or sys.platform.startswith('cygwin'):
+        # this excludes your current terminal "/dev/tty"
+        ports = glob.glob('/dev/tty[A-Za-z]*')
+    elif sys.platform.startswith('darwin'):
+        ports = glob.glob('/dev/tty.*')
+    else:
+        raise EnvironmentError('Unsupported platform')
+
+    for port in ports:
+        try:
+            se = Serial(port)
+            se.close()
+            return port
+        except (OSError, SerialException):
+            pass
+
+    return ""
+
+
+s_port: str = serial_ports()
+if s_port:
+    print(s_port + ' will be used')
+else:
+    print('No serial port found')
+    exit()
+
+s = Serial(s_port, 9600, timeout=1)
+apiKey: str = '1VP9BWGNWA91KDHU'
+
+while True:
+    s.write(b'T')
+    res = s.readline()
+    if res:
+      try:
+         temp = float(res)
+         print(temp)
+         time.sleep(1)
+
+      except (KeyboardInterrupt, SystemExit):
+         raise
+      except:
+         raise
+
