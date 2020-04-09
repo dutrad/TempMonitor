aed96ed70e3b150196bdc03e6e6cd581c392fe02
dutrad.vinicius <dutrad.vinicius@gmail.com>
2020-04-09 14:30:29 +0100

    Fourier of temperature signal

--- Filtro/FFT.py
+++ Filtro/FFT.py
@@ -1,15 +1,27 @@
+import numpy as np
 from scipy.fft import fft
 import matplotlib.pyplot as plt
+import csv
+
+with open('feeds.csv', newline='') as csvfile:
+    data = list(csv.reader(csvfile))
 
 # Number of sample points
-N = 600
+N = len(data)
 # sample spacing
-T = 1.0 / 800.0
+T = 30
 x = np.linspace(0.0, N * T, N)
-y = np.sin(50.0 * 2.0 * np.pi * x) + 0.5 * np.sin(80.0 * 2.0 * np.pi * x)
+y = data
 yf = fft(y)
+
 xf = np.linspace(0.0, 1.0 / (2.0 * T), N // 2)
 
+# Plot
+plt.figure(1)
 plt.plot(xf, 2.0 / N * np.abs(yf[0:N // 2]))
+plt.grid()
+
+plt.figure(2)
+plt.plot(np.array(y, dtype=np.float))
 plt.grid()
 plt.show()
