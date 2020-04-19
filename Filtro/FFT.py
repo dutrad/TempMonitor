0cb7d720903d8a77eabccd1e4740cdb8ddcee211
dutrad.vinicius <dutrad.vinicius@gmail.com>
2020-04-19 12:51:46 +0100

    New data and filter

--- Filtro/FFT.py
+++ Filtro/FFT.py
@@ -1,5 +1,5 @@
 import numpy as np
-from scipy.fft import fft
+from scipy.fft import fft, ifft
 from scipy import signal
 import matplotlib.pyplot as plt
 import pandas as pd
@@ -13,17 +13,21 @@ N = len(y)
 T = 30
 x = np.linspace(0.0, N * T, N)
 yf = fft(y)
-xf = np.linspace(0.0, 1.0 / (2.0 * T), N // 2)
+xf = np.linspace(0.0, 1.0 / (2.0 * T), N)
 
 # Filter
-b, a = signal.butter(3, 0.008, 'low')
-y_filter = signal.filtfilt(b, a, y, padlen=0)
-yf_filter = fft(y_filter)
+#b, a = signal.butter(8, 0.01)
+#y_filter = signal.filtfilt(b, a, y, padlen=0)
+#yf_filter = fft(y_filter)
+
+y_filter = yf
+y_filter[N // 2: N] = 0
+yf_filter = ifft(y_filter)
 
 # Plot
 plt.figure(1)
-plt.plot(xf, 2.0 / N * np.abs(yf[0:N // 2]), 'k-', label='Signal')
-plt.plot(xf, 2.0 / N * np.abs(yf_filter[0:N // 2]),  'b-', label='Filtered')
+plt.plot(xf, 2.0 / N * np.abs(yf[0:N]), 'k-', label='Signal')
+plt.plot(xf, 2.0 / N * np.abs(yf_filter[0:N]), 'b-', label='Filtered')
 plt.grid()
 plt.legend(loc='best')
 plt.xlabel('Hz')
