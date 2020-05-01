2fc6d06973083c1be17b5a2472980fa828e33690
dutrad.vinicius <dutrad.vinicius@gmail.com>
2020-05-01 12:13:51 +0100

    Latest

--- Filtro/FFT.py
+++ Filtro/FFT.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 y = pd.read_csv('feeds.csv', usecols=['field1'])
-y = np.array(y[100:len(y)], dtype=np.float)
+y = np.array(y, dtype=np.float)
 # Number of sample points
 N = len(y)
 
@@ -15,29 +15,4 @@ x = np.linspace(0.0, N * T, N)
 yf = fft(y)
 xf = np.linspace(0.0, 1.0 / (2.0 * T), N)
 
-# Filter
-#b, a = signal.butter(8, 0.01)
-#y_filter = signal.filtfilt(b, a, y, padlen=0)
-#yf_filter = fft(y_filter)
 
-y_filter = yf
-y_filter[N // 2: N] = 0
-yf_filter = ifft(y_filter)
-
-# Plot
-plt.figure(1)
-plt.plot(xf, 2.0 / N * np.abs(yf[0:N]), 'k-', label='Signal')
-plt.plot(xf, 2.0 / N * np.abs(yf_filter[0:N]), 'b-', label='Filtered')
-plt.grid()
-plt.legend(loc='best')
-plt.xlabel('Hz')
-plt.ylabel('|Y|')
-
-plt.figure(2)
-plt.plot(np.array(y, dtype=np.float), 'k-', label='Signal')
-plt.plot(y_filter, 'b-', label='Filtered')
-plt.legend(loc='best')
-plt.grid()
-plt.ylabel('Temperature (ºC)')
-
-plt.show()
