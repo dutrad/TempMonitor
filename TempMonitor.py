dcfab7a955b5e0d43aa3e50ce38ab01c8b526fab
root <dutrad.vincius@gmail.com>
2020-05-20 16:54:25 +0100

    Add moving average filter

--- TempMonitor.py
+++ TempMonitor.py
@@ -5,6 +5,20 @@ import urllib.request
 from serial import Serial, SerialException
 from serialPort import serial_ports
 
+def firstTemps(serialP):
+    t0,t1 = 0.0
+    while True:
+       serialP.write(b'T')
+       res = serialP.readline()
+      
+       if res:
+           if t0 == 0.0
+              t0 = float(res)
+              print(t0)
+           else
+              t1 = float(res)
+              print(t1)
+              return [t0,t1]
 
 s_port: str = serial_ports()
 while s_port == "":
@@ -17,12 +31,17 @@ print(s_port + ' will be used')
 s = Serial(s_port, 9600, timeout=1)
 apiKey: str = '1VP9BWGNWA91KDHU'
 
+[t0,t1] = firstTemps(s)
+
 while True:
     s.write(b'T')
     res = s.readline()
     if res:
-        temp = float(res)
+        temp = (float(res) + t0 + t1)/3
         print(temp)
+
+        t0 = t1
+        t1 = temp
 
         try:
             params = urllib.parse.urlencode({'key': apiKey, 'field1': temp}).encode('ascii')
